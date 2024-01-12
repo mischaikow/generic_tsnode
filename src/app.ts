859e19da981dc6a3ee3029725e2e5f12b0b3f87c
mischaikow <mischaik@gmail.com>
2024-01-12 11:38:51 -0500

    bug fixes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.json());
 
+app.set('port', 3000);
+
 app.get('/', (req, res) => {
   return res.send('Hello world!\n');
 });
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import app from './app.js';
 
 const server = app.listen(app.get('port'), () => {
-  const PORT: number = 3000;
-  console.log(`  App is running at http://localhost:${PORT} in %s mode\n`);
+  console.log(`  App is running at http://localhost:${app.get('port')}\n`);
   console.log('  Press CTRL-C to stop\n');
 });
 

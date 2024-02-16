7a11b7dd3bbb4ee11b1c2cbf03084e18474c3c8a
mischaikow <mischaik@gmail.com>
2024-02-15 23:04:06 -0500

    formatting

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest';
 import { dummy } from './app.js';
 
-  test('does vitest work', () => {
-    expect(dummy(2)).toBe(3);
-  });
+test('does vitest work', () => {
+  expect(dummy(2)).toBe(3);
+});
